9e3d4d9aeb518fc72aa35dbc6b7255b96211803f
pavelmashina <pashakriklivetc@gmail.com>
2026-08-19 14:33:20 +0300

    Slightly enlarge training tab icons
    * Slightly enlarge training tab icons
    
    * Load larger training icon adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './styles.css';
 import './auth.css';
 import './training-icon-tabs.css';
+import './training-icon-tabs-adjustments.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
